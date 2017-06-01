996d6319d242da29f6b0fb4ffc11192191116aa4
ganeshi4u <ganeshvarmai4u@gmail.com>
2017-06-01 19:00:12 +0530

    bringup aospa

--- XOS.mk
+++
@@ -1,8 +0,0 @@
-# Inherit some common stuff.
-$(call inherit-product, vendor/xos/config/common.mk)
-
-$(call inherit-product, device/samsung/j5ltexx/full_j5ltexx.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=j5ltexx TARGET_DEVICE=j5lte
-
-PRODUCT_NAME := XOS_j5ltexx
